87e2b41e7e53af6d5676ad86c7d46eb8dd1e5de5
York Wei <york.wei@icloud.com>
2021-06-03 09:59:45 -0400

    Color constants

---
+++ TeachAssist/TAColor.swift
@@ -0,0 +1,12 @@
+//
+//  TAColor.swift
+//  TeachAssist
+//
+//  Created by York Wei on 2021-06-03.
+//
+
+import SwiftUI
+
+struct TAColor {
+    static let backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+}
