90bb61fc9de552181f55cba36e86b02fb7799c4d
York Wei <york.wei@icloud.com>
2021-06-03 00:29:45 -0400

    Add Keychain to UserState

--- TeachAssist/UserState.swift
+++ TeachAssist/UserState.swift
@@ -20,15 +20,15 @@ class UserState: ObservableObject {
     
     @Published var isLoading: Bool = false
 
-    @Published var username: String = UserDefaults.standard.string(forKey: "USERNAME") ?? "" {
+    @Published var username: String = KeychainWrapper.standard.string(forKey: "USERNAME") ?? "" {
         didSet {
-            UserDefaults.standard.set(username, forKey: "USERNAME")
+            KeychainWrapper.standard.set(username, forKey: "USERNAME")
         }
     }
     
-    @Published var password: String = UserDefaults.standard.string(forKey: "PASSWORD") ?? "" {
+    @Published var password: String = KeychainWrapper.standard.string(forKey: "PASSWORD") ?? "" {
         didSet {
-            UserDefaults.standard.set(password, forKey: "PASSWORD")
+            KeychainWrapper.standard.set(password, forKey: "PASSWORD")
         }
     }
     
