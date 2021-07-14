15165955e4d0951efdee0901b867e2440a41bdc4
York Wei <york.wei@icloud.com>
2021-07-14 12:52:57 -0400

    Move secure text field

---
+++ TeachAssist/ComponentViews/SecureTextField.swift
@@ -0,0 +1,60 @@
+//
+//  SecureTextField.swift
+//  TeachAssist
+//
+//  Created by York Wei on 2021-07-14.
+//
+
+import SwiftUI
+
+struct SecureTextField: UIViewRepresentable {
+    
+    let title: String
+    @Binding var text: String
+    var onEditingChanged: (Bool) -> Void
+    
+    func makeUIView(context: Context) -> UITextField {
+        let view = UITextField()
+        view.placeholder = title
+        view.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.footnote)
+        view.textColor = UIColor(named: "PrimaryTextColor")
+        view.delegate = context.coordinator
+        view.isSecureTextEntry = true
+        view.endEditing(true)
+        view.keyboardType = UIKeyboardType.asciiCapable
+        return view
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        DispatchQueue.main.async {
+            uiView.text = text
+        }
+    }
+    
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var parent: SecureTextField
+        
+        init(parent: SecureTextField) {
+            self.parent = parent
+        }
+        
+        func textFieldDidBeginEditing(_ textField: UITextField) {
+            parent.onEditingChanged(true)
+        }
+        
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            parent.onEditingChanged(false)
+            if let text = textField.text {
+                self.parent.text = text
+            }
+        }
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            textField.resignFirstResponder()
+        }
+    }
+}
--- TeachAssist/Views/LoginView.swift
+++ TeachAssist/Views/LoginView.swift
@@ -10,9 +10,7 @@ import SwiftUI
 
 struct LoginView: View {
         
-    @State var fadeIn = true
-    @State var selectedUsername = false
-    @State var selectedPassword = false
+    @ObservedObject var viewModel: ViewModel
     
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
@@ -31,64 +29,4 @@ extension LoginView {
             self.userState = userState
         }
     }
-}
-
-struct LoginView_Previews: PreviewProvider {
-    static var previews: some View {
-        LoginView()
-    }
-}
-
-struct SecureTextField: UIViewRepresentable {
-    
-    var title: String
-    @Binding var text: String
-    var onEditingChanged: (Bool) -> Void
-    
-    func makeUIView(context: Context) -> UITextField {
-        let view = UITextField()
-        view.placeholder = title
-//        view.font = UIFont.preferredFont(forTextStyle: UIFont()
-//        view.textColor = UIColor(Color("PrimaryTextColor"))
-        view.delegate = context.coordinator
-        view.isSecureTextEntry = true
-        view.endEditing(true)
-        view.keyboardType = UIKeyboardType.asciiCapable
-        return view
-    }
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator(p: self)
-    }
-    
-    func updateUIView(_ uiView: UITextField, context: Context) {
-        DispatchQueue.main.async {
-            uiView.text = text
-        }
-    }
-    
-    class Coordinator: NSObject, UITextFieldDelegate {
-        
-        var parent: SecureTextField
-        
-        init(p: SecureTextField) {
-            self.parent = p
-        }
-        
-        func textFieldDidBeginEditing(_ textField: UITextField) {
-            parent.onEditingChanged(true)
-        }
-        
-        func textFieldDidEndEditing(_ textField: UITextField) {
-            parent.onEditingChanged(false)
-            if let text = textField.text {
-                self.parent.text = text
-            }
-        }
-        
-        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-            textField.resignFirstResponder()
-        }
-    }
-    
 }
