f2dc5ea2ede60ea5a99ac48a345bd46499ce75e3
York Wei <york.wei@icloud.com>
2021-09-03 22:00:37 -0400

    Update CourseView.swift

--- TeachAssist/Views/CourseView.swift
+++ TeachAssist/Views/CourseView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CourseView: View {
     @Binding var show: Bool
+    @State var currentOffsetX: CGFloat = 0
     @StateObject var viewModel: ViewModel
     
     var body: some View {
@@ -16,6 +17,27 @@ struct CourseView: View {
             
         }
         .background(TAColor.backgroundColor)
+        .offset(x: currentOffsetX)
+        .gesture(DragGesture(coordinateSpace: .global)
+                    .onChanged { value in
+                        if value.translation.width > 0 {
+                            currentOffsetX = value.translation.width
+                        }
+                    }
+                    .onEnded { value in
+                        if currentOffsetX > UIScreen.main.bounds.width / 2 || value.predictedEndLocation.x - value.location.x > 250 {
+                            withAnimation {
+                                currentOffsetX = UIScreen.main.bounds.width
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                                    self.show = false
+                                }
+                            }
+                        } else {
+                            withAnimation {
+                                currentOffsetX = 0
+                            }
+                        }
+                    })
     }
 }
 
